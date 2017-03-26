dfc8a396c7609ae4ddb51a2a43538617f979d518
ddobson <ddobson445@gmail.com>
2017-03-26 13:41:39 -0400

    React-Dom-Router
    - Add react router and set App as component for "/"

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import App from './components/App';
 import './styles/index.css';
 
 ReactDOM.render(
-  <App />,
+  <div>
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
+  </div>,
   document.getElementById('root')
 );
