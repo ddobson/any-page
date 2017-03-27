1b5f47f813d2b98b422dea01e8e3a14f1f089b0f
ddobson <ddobson445@gmail.com>
2017-03-27 16:54:40 -0400

    Fix typo in API prod path

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   production: {
-    api: "https://cook-book-nook-api.herokuapp.com/"
+    api: "https://cook-book-nook-api.herokuapp.com"
   },
   development: {
     api: "http://localhost:4741"
