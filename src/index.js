36122872f900a58228a0477f0be035c8ee16ca75
ddobson <ddobson445@gmail.com>
2017-03-28 10:53:01 -0400

    Replace Bootstrap Grid with Spectre
    - Replace the CSS grid system

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './components/App';
-import './styles/bootstrap.min.css'
+import './styles/spectre.min.css'
 import './styles/index.css';
 
 injectTapEventPlugin();
