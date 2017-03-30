81b2ddb2ebea000867eb0a81c37a69c40291f9d4
ddobson <ddobson445@gmail.com>
2017-03-30 19:21:23 -0400

    Add a 404 route
    - Add a 404 route and component
    - Remove unneeded 200.html

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 
 import Welcome from './Welcome';
 import CookbookMenu from './CookbookMenu';
@@ -9,61 +9,68 @@ import SignUpForm from './forms/SignUpForm';
 import SignInForm from './forms/SignInForm';
 import ChangePwForm from './forms/ChangePwForm';
 import RecipeDetail from './RecipeDetail';
+import NotFound from './NotFound';
 
 class Main extends React.Component {
   render() {
     return (
       <div id='main'>
-        <Route exact={true} path='/' component={Welcome} />
-        <Route exact={true} path='/welcome' component={Welcome} />
-        <Route exact={true} path='/cookbooks' render={props => (
-          this.props.loggedIn ? (
-            <CookbookMenu {...props} loggedIn={ this.props.loggedIn } />
-          ) : (
+        <Switch>
+          <Route exact={true} path='/' component={Welcome} />
+          <Route exact={true} path='/welcome' component={Welcome} />
+          <Route exact={true} path='/cookbooks' render={props => (
+            this.props.loggedIn ? (
+              <CookbookMenu {...props} loggedIn={ this.props.loggedIn } />
+            ) : (
+              <Redirect to='/welcome'/>
+            )
+          )}/>
+          <Route exact={true} path='/cookbooks/:id' render={props => (
+            this.props.loggedIn ? (
+              <CookbookDetail {...props} loggedIn={ this.props.loggedIn } />
+            ) : (
+              <Redirect to='/welcome'/>
+            )
+          )}/>
+        <Route exact={true} path='/cookbooks/:id/edit' render={props => (
+            this.props.loggedIn ? (
+              <EditCookbook {...props} loggedIn={ this.props.loggedIn } />
+            ) : (
+              <Redirect to='/welcome'/>
+            )
+          )}/>
+          <Route exact={true} path='/recipes/:id' component={RecipeDetail} />
+          <Route exact path='/sign-up' render={props => (
+            this.props.loggedIn ? (
+              <Redirect {...props} to='/cookbooks'/>
+            ) : (
+              <SignUpForm handleAuth={ this.props.handleAuth } updateAuthStatus={ this.props.updateAuthStatus }/>
+            )
+          )}/>
+          <Route exact path='/sign-in' render={props => (
+            this.props.loggedIn ? (
+              <Redirect {...props} to='/cookbooks'/>
+            ) : (
+              <SignInForm handleAuth={ this.props.handleAuth } updateAuthStatus={ this.props.updateAuthStatus }/>
+            )
+          )}/>
+          <Route exact={true} path='/change-pw' render={() => (
+            <ChangePwForm handleAuth={ this.props.handleAuth } />
+          )}/>
+          <Route exact={true} path='/sign-out' render={() => (
             <Redirect to='/welcome'/>
-          )
-        )}/>
-        <Route exact={true} path='/cookbooks/:id' render={props => (
-          this.props.loggedIn ? (
-            <CookbookDetail {...props} loggedIn={ this.props.loggedIn } />
-          ) : (
+          )}/>
+          <Route exact={true} path='/cookbooks/sign-out' render={() => (
             <Redirect to='/welcome'/>
-          )
-        )}/>
-      <Route exact={true} path='/cookbooks/:id/edit' render={props => (
-          this.props.loggedIn ? (
-            <EditCookbook {...props} loggedIn={ this.props.loggedIn } />
-          ) : (
+          )}/>
+          <Route exact={true} path='/recipes/sign-out' render={() => (
             <Redirect to='/welcome'/>
-          )
-        )}/>
-        <Route exact={true} path='/recipes/:id' component={RecipeDetail} />
-        <Route exact path='/sign-up' render={props => (
-          this.props.loggedIn ? (
-            <Redirect {...props} to='/cookbooks'/>
-          ) : (
-            <SignUpForm handleAuth={ this.props.handleAuth } updateAuthStatus={ this.props.updateAuthStatus }/>
-          )
-        )}/>
-        <Route exact path='/sign-in' render={props => (
-          this.props.loggedIn ? (
-            <Redirect {...props} to='/cookbooks'/>
-          ) : (
-            <SignInForm handleAuth={ this.props.handleAuth } updateAuthStatus={ this.props.updateAuthStatus }/>
-          )
-        )}/>
-        <Route exact={true} path='/change-pw' render={() => (
-          <ChangePwForm handleAuth={ this.props.handleAuth } />
-        )}/>
-        <Route exact={true} path='/sign-out' render={() => (
-          <Redirect to='/welcome'/>
-        )}/>
-        <Route exact={true} path='/cookbooks/sign-out' render={() => (
-          <Redirect to='/welcome'/>
-        )}/>
-        <Route exact={true} path='/recipes/sign-out' render={() => (
-          <Redirect to='/welcome'/>
-        )}/>
+          )}/>
+          <Route exact={true} path='/cookbooks/:id/sign-out' render={() => (
+            <Redirect to='/welcome'/>
+          )}/>
+        <Route component={NotFound} />
+        </Switch>
       </div>
     )
   }
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div className='container'>
+        <div className='columns'>
+          <div className='column col-xs-12'>
+            <h1 className='center'>404 NotFound</h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NotFound;
