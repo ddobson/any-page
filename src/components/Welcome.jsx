5f00a1012a937653ee192ebac1db0fea327e0b7f
ddobson <ddobson445@gmail.com>
2017-03-30 11:09:34 -0400

    Add text to welcome component
    Add an app description to the welcome component

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 class Welcome extends React.Component {
   render() {
     return (
-      <h1>WELCOME COMPONENT</h1>
+      <div className='container'>
+        <div className='columns'>
+          <div className='column col-xs-12'>
+            <h1 className='center'>Welcome to Cookbook Nook</h1>
+            <p className='center'>Coobook Nook is a place to store your cookbook collection and get random recipe suggestions.</p>
+            <p className='center'>Enter the pages in your cookbook where recipes occur and we'll give you a random page.</p>
+            <p className='center'>If you like the recipe, save it and we'll never suggest that one again. We promise!</p>
+          </div>
+        </div>
+      </div>
     )
   }
 }
